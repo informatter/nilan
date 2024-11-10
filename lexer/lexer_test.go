8631845947023ddd563ce90b1781e0483afe451c
informatter <nicholas.rawitscher@gmail.com>
2024-11-10 21:13:54 +0000

    Abstracted running the actual test code in to its own method so it can be easily re-used accross other test methods

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,45 @@ import (
 	"testing"
 )
 
-func TestScan(t *testing.T) {
+
+func runTestSuccess(t *testing.T, scanner *Lexer, expected []token.Token){
+
+	t.Run("ValidTokenScan", func(t *testing.T) {
+		got, err := scanner.Scan()
+		if err != nil {
+			t.Errorf("scanner.Scan() raised an error: %v", err)
+		}
+
+		// Use reflect.DeepEqual to compare the slices
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("scanner.Scan() = %v, want %v", got, expected)
+		}
+	})
+}
+
+func TestOperatorsSuccess(t *testing.T){
+	expected := []token.Token{
+		token.CreateToken(token.EQUAL_EQUAL),
+		token.CreateToken(token.DIV),
+		token.CreateToken(token.ASSIGN),
+		token.CreateToken(token.MULT),
+		token.CreateToken(token.ADD),
+		token.CreateToken(token.LARGER),
+		token.CreateToken(token.SUB),
+		token.CreateToken(token.LESS),
+		token.CreateToken(token.NOT_EQUAL),
+		token.CreateToken(token.LESS_EQUAL),
+		token.CreateToken(token.LARGER_EQUAL),
+		token.CreateToken(token.BANG),
+		token.CreateToken(token.BANG),
+		token.CreateToken(token.EOF),
+	}
+	scanner := CreateLexer("==/=*+>-<!=<=>=!!")
+	runTestSuccess(t,scanner,expected)
+
+}
+
+func TestScanSuccess(t *testing.T) {
 	expected := []token.Token{
 		token.CreateToken(token.LPA),
 		token.CreateToken(token.RPA),
@@ -22,16 +60,6 @@ func TestScan(t *testing.T) {
 	}
 
 	scanner := CreateLexer("(){}**;+!=<=")
+	runTestSuccess(t,scanner,expected)
 
-	t.Run("ValidTokenScan", func(t *testing.T) {
-		got, err := scanner.Scan()
-		if err != nil {
-			t.Errorf("scanner.Scan() raised an error: %v", err)
-		}
-
-		// Use reflect.DeepEqual to compare the slices
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("scanner.Scan() = %v, want %v", got, expected)
-		}
-	})
 }
