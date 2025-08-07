415f4466643083594efd851289a548ace0906e0e
informatter <nicholas.rawitscher@gmail.com>
2025-08-07 21:47:37 +0100

    refactor: updated return types from all methods in the astPrinter class from string to any

--- parser/printer.go
+++ parser/printer.go
@@ -8,26 +8,27 @@ import (
 // and prints an Abstract Syntax Tree (AST)
 type astPrinter struct{}
 
-func (p astPrinter) VisitBinary(b Binary) string {
+func (p astPrinter) VisitBinary(b Binary) any {
 
 	return parenthesize(b.Operator.Value, b.Left, b.Right)
 }
-func (p astPrinter) VisitUnary(u Unary) string {
+func (p astPrinter) VisitUnary(u Unary) any {
 	return parenthesize(u.Operator.Value, u.Right)
 }
-func (p astPrinter) VisitLiteral(l Literal) string {
+func (p astPrinter) VisitLiteral(l Literal) any {
 	return fmt.Sprintf("%v", l.Value)
 }
-func (p astPrinter) VisitGrouping(g Grouping) string {
+func (p astPrinter) VisitGrouping(g Grouping) any {
 	return parenthesize("group", g.Expression)
 }
 
 // parenthesize creates an S-expression in order to visualise
 // the expression presedence order within the AST.
 func parenthesize(name string, expressions ...Expression) string {
+	// var printer Visitor[string] = astPrinter{}
 	astString := "(" + name
 	for _, expression := range expressions {
-		astString += " " + expression.Accept(astPrinter{})
+		astString += " " + expression.Accept(astPrinter{}).(string)
 	}
 	astString += ")"
 	return astString
