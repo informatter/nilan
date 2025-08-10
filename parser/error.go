3f8a71c5b77f8bb982c8222be80e77bb874927fb
informatter <nicholas.rawitscher@gmail.com>
2025-08-10 16:23:00 +0100

    feat: Added SyntaxError struct to the parser package

---
+++ parser/error.go
@@ -0,0 +1,22 @@
+package parser
+import "fmt"
+
+// Defines the struct for all syntax errors in the Parser
+type SyntaxError struct {
+    Line int32
+	Column int
+    Message string
+}
+
+func CreateSyntaxError(line int32, column int, message string) SyntaxError{
+	return SyntaxError{
+		Line: line,
+		Column: column,
+		Message: message,
+	}
+}
+
+
+func (e SyntaxError) Error() string {
+    return fmt.Sprintf("💥 Nilan Syntax error:\nline:%d, column:%d - %s", e.Line, e.Column, e.Message)
+}
