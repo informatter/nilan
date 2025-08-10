e42f557633100465c4e1f7ca784a0afff6389dd9
informatter <nicholas.rawitscher@gmail.com>
2025-08-10 16:20:06 +0100

    feat: Added RuntimeError struct to the interpreter package

---
+++ interpreter/error.go
@@ -0,0 +1,22 @@
+package interpreter
+import "fmt"
+
+// Defines the struct for all runtime errors in the Parser
+type RuntimeError struct {
+    Line int32
+	Column int
+    Message string
+}
+
+func CreateRuntimeError(line int32, column int, message string) RuntimeError{
+	return RuntimeError{
+		Line: line,
+		Column: column,
+		Message: message,
+	}
+}
+
+
+func (e RuntimeError) Error() string {
+    return fmt.Sprintf("💥 Nilan Runtime error:\nline:%d, column:%d - %s", e.Line, e.Column, e.Message)
+}
