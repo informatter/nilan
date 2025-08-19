9227ff5237a65e51ced2a73b1b0cf371b0d2f5cc
informatter <nicholas.rawitscher@gmail.com>
2025-08-19 21:58:09 +0100

    feat: Updated the AST printer to handle expressio and print statements by implementing the StmtVisitor interface

--- parser/printer.go
+++ parser/printer.go
@@ -8,6 +8,15 @@ import (
 // and prints an Abstract Syntax Tree (AST)
 type astPrinter struct{}
 
+func (p astPrinter) VisitExpressionStmt(exprStmt ExpressionStmt) any {
+	return parenthesize("dd", exprStmt.Expression)
+}
+
+func (p astPrinter) VisitPrintStmt(printStmt PrintStmt) any {
+
+	return parenthesize("print", printStmt.Expression)
+}
+
 func (p astPrinter) VisitBinary(b Binary) any {
 
 	return parenthesize(b.Operator.Lexeme, b.Left, b.Right)
@@ -25,7 +34,6 @@ func (p astPrinter) VisitGrouping(g Grouping) any {
 // parenthesize creates an S-expression in order to visualise
 // the expression presedence order within the AST.
 func parenthesize(name string, expressions ...Expression) string {
-	// var printer Visitor[string] = astPrinter{}
 	astString := "(" + name
 	for _, expression := range expressions {
 		astString += " " + expression.Accept(astPrinter{}).(string)
