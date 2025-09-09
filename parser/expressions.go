3803e1e09507d51d9865e192468caddb033676e6
informatter <nicholas.rawitscher@gmail.com>
2025-09-09 23:19:21 +0100

    feat: Added Assign expression AST node to model the operation of assigning a new value to an existing variable

--- parser/expressions.go
+++ parser/expressions.go
@@ -67,6 +67,29 @@ type Variable struct {
 	Name token.Token // An IDENTIFIER token
 }
 
+// Variable represents a variable expression in the abstract syntax tree (AST).
+// It models the retrieval of a value previously bound to a variable name.
+//
+// Fields:
+//   - Name: The token corresponding to the variable's identifier. This is an
+//     IDENTIFIER token that holds the variable's name (lexeme).
 func (variable Variable) Accept(v Visitor) any {
 	return v.VisitVariableExpression(variable)
+}
+
+// Assign represents an assignment expression in the abstract syntax tree (AST).
+// It models the operation of assigning a new value to an existing variable.
+//
+// Fields:
+//   - Name: The token corresponding to the variable's identifier.
+//   - Value: The expression that produces the value being assigned to the variable.
+//     This can be any valid expression node in the AST, which will be
+//     evaluated and then stored in the environment.
+type Assign struct {
+	Name  token.Token
+	Value Expression
+}
+
+func (assign Assign) Accept(v Visitor) any {
+	return v.VisitAssignExpression(assign)
 }
