9b665b46565907a16447f956cb1e09f27b73a61e
informatter <nicholas.rawitscher@gmail.com>
2025-09-21 18:16:53 +0100

    refactor: Modified to inlcude subcommands usage and register the REPL and Run commands for CLI usage

--- main.go
+++ main.go
@@ -1,48 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"nilan/interpreter"
-	"nilan/lexer"
-	"nilan/parser"
+	"context"
+	"flag"
+	"github.com/google/subcommands"
 	"os"
 )
 
 func main() {
-	fmt.Println("\n\nWelcome to Nilan!")
-	repl(os.Stdin, os.Stdout)
-}
-
-func repl(in io.Reader, out io.Writer) {
-
-	scanner := bufio.NewScanner(in)
-	interpreter := interpreter.Make()
-
-	for {
-		fmt.Fprintf(out, ">>> ")
-		scanned := scanner.Scan()
-		if !scanned {
-			return
-		}
-		line := scanner.Text()
-		if line == "exit" {
-			os.Exit(0)
-		}
-		lex := lexer.CreateLexer(line)
-		tokens, err := lex.Scan()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		parser := parser.Make(tokens)
-		ast, errors := parser.Parse()
-		if len(errors) > 0 {
-			continue
-		}
-		parser.Print(ast)
-		interpreter.Interpret(ast)
-
-	}
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&runCmd{}, "")
+	subcommands.Register(&replCmd{}, "")
+	flag.Parse()
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
 }
