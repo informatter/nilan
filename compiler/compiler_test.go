37f59646b2371cafbf97f943b44fd58344e3f549
informatter <nicholas.rawitscher@gmail.com>
2025-09-30 23:55:34 +0100

    test: Updated TestCompileNumericTokens so it takes into account the OP_END opcode

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -15,13 +15,14 @@ func TestCompileNumericTokens(t *testing.T) {
 			tokens: []token.Token{
 				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
 				token.CreateLiteralToken(token.INT, int64(1), "1", 0, 0),
+				token.CreateToken(token.EOF, 0, 0),
 			},
 			expectedBytecode: Bytecode{
 				// NOTE: expected instruction operands are the indices in the constants pool
 				// where 5 and 1 where added, encoded in Big Endian. In this case 5 is added
 				// to index 0 and 1 to index 1. 0 in Big Endian uint16 is represented as 0,0 (decimal)
 				// and 1 as 0,1 (decimal)
-				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1},
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_END)},
 				ConstantsPool: []any{int64(5), int64(1)},
 			},
 		},
