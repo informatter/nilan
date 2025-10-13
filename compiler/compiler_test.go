0e20e20c5926245c2d881a74487592ed491a0cdd
informatter <nicholas.rawitscher@gmail.com>
2025-10-13 21:46:53 +0100

    test: Add simple initial tests for the compiler to verify the fucntionality for parsing and compiling binary and unary expressions

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -5,8 +5,20 @@ import (
 	"testing"
 )
 
-func TestCompileNumericTokens(t *testing.T) {
+func assertBytecodeEquals(t *testing.T, got Bytecode, want Bytecode) {
+	for i, instruction := range got.Instructions {
+		if instruction != want.Instructions[i] {
+			t.Errorf("expected instruction does not equal computed instruction at index %d", i)
+		}
+	}
+	for i, constant := range got.ConstantsPool {
+		if constant != want.ConstantsPool[i] {
+			t.Errorf("expected constant does not equal computed constant at index %d - want: %v, got: %v", i, want.ConstantsPool[i], constant)
+		}
+	}
+}
 
+func TestCompileNumericTokens_BinaryExpressions(t *testing.T) {
 	tests := []struct {
 		tokens           []token.Token
 		expectedBytecode Bytecode
@@ -14,41 +26,87 @@ func TestCompileNumericTokens(t *testing.T) {
 		{
 			tokens: []token.Token{
 				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+				token.CreateToken(token.ADD, 0, 0),
 				token.CreateLiteralToken(token.INT, int64(1), "1", 0, 0),
 				token.CreateToken(token.EOF, 0, 0),
 			},
 			expectedBytecode: Bytecode{
-				// NOTE: expected instruction operands are the indices in the constants pool
-				// where 5 and 1 where added, encoded in Big Endian. In this case 5 is added
-				// to index 0 and 1 to index 1. 0 in Big Endian uint16 is represented as 0,0 (decimal)
-				// and 1 as 0,1 (decimal)
-				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_END)},
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_ADD), byte(OP_END)},
+				ConstantsPool: []any{int64(5), int64(1)},
+			},
+		},
+		{
+			tokens: []token.Token{
+				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+				token.CreateToken(token.MULT, 0, 0),
+				token.CreateLiteralToken(token.INT, int64(1), "1", 0, 0),
+				token.CreateToken(token.EOF, 0, 0),
+			},
+			expectedBytecode: Bytecode{
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_MULTIPLY), byte(OP_END)},
+				ConstantsPool: []any{int64(5), int64(1)},
+			},
+		},
+		{
+			tokens: []token.Token{
+				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+				token.CreateToken(token.DIV, 0, 0),
+				token.CreateLiteralToken(token.INT, int64(1), "1", 0, 0),
+				token.CreateToken(token.EOF, 0, 0),
+			},
+			expectedBytecode: Bytecode{
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_DIVIDE), byte(OP_END)},
+				ConstantsPool: []any{int64(5), int64(1)},
+			},
+		},
+		{
+			tokens: []token.Token{
+				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+				token.CreateToken(token.SUB, 0, 0),
+				token.CreateLiteralToken(token.INT, int64(1), "1", 0, 0),
+				token.CreateToken(token.EOF, 0, 0),
+			},
+			expectedBytecode: Bytecode{
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_CONSTANT), 0, 1, byte(OP_SUBTRACT), byte(OP_END)},
 				ConstantsPool: []any{int64(5), int64(1)},
 			},
 		},
 	}
 
 	for _, tt := range tests {
-
-		compiler := NewCompiler()
-		bytecode, err := compiler.Compile(tt.tokens)
+		compiler := NewCompiler(tt.tokens)
+		bytecode, err := compiler.Compile()
 		if err != nil {
 			t.Errorf("compilation error occurred: %s", err.Error())
 		}
+		assertBytecodeEquals(t, bytecode, tt.expectedBytecode)
+	}
+}
 
-		for i, instruction := range bytecode.Instructions {
-			expectedInstruction := tt.expectedBytecode.Instructions[i]
-			if expectedInstruction != instruction {
-				t.Errorf("expected instruction does not equal computed instruction")
-			}
+func TestCompileNumericTokens_UnaryExpressions(t *testing.T) {
+	tests := []struct {
+		tokens           []token.Token
+		expectedBytecode Bytecode
+	}{
+		{
+			tokens: []token.Token{
+				token.CreateToken(token.SUB, 0, 0),
+				token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+				token.CreateToken(token.EOF, 0, 0),
+			},
+			expectedBytecode: Bytecode{
+				Instructions:  []byte{byte(OP_CONSTANT), 0, 0, byte(OP_NEGATE), byte(OP_END)},
+				ConstantsPool: []any{int64(5)},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		compiler := NewCompiler(tt.tokens)
+		bytecode, err := compiler.Compile()
+		if err != nil {
+			t.Errorf("compilation error occurred: %s", err.Error())
 		}
-
-		for i, constant := range bytecode.ConstantsPool {
-			expectedConstant := tt.expectedBytecode.ConstantsPool[i]
-			if constant != expectedConstant {
-				t.Errorf("expected constant does not equal computed constant - want: %v, got: %v", expectedConstant, constant)
-			}
-		}
-
+		assertBytecodeEquals(t, bytecode, tt.expectedBytecode)
 	}
 }
