245d096b309619c0375d17432ec02b22fbc7b95d
informatter <nicholas.rawitscher@gmail.com>
2025-10-18 10:27:13 +0100

    test: Add TestDiassembleBytecode and enhanced assertBytecodeEquals helper function to ensure that the length of the computed instructions have the length of the expected instructions.

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -2,18 +2,24 @@ package compiler
 
 import (
 	"nilan/token"
+	"strings"
 	"testing"
 )
 
 func assertBytecodeEquals(t *testing.T, got Bytecode, want Bytecode) {
+
+	if len(got.Instructions) != len(want.Instructions) {
+		t.Errorf("computed instructions has a different length than the expected instructions - got: %d, want: %d", len(got.Instructions), len(want.Instructions))
+	}
+
 	for i, instruction := range got.Instructions {
 		if instruction != want.Instructions[i] {
-			t.Errorf("expected instruction does not equal computed instruction at index %d", i)
+			t.Errorf("computed instruction does not equal expected instruction at index %d", i)
 		}
 	}
 	for i, constant := range got.ConstantsPool {
 		if constant != want.ConstantsPool[i] {
-			t.Errorf("expected constant does not equal computed constant at index %d - want: %v, got: %v", i, want.ConstantsPool[i], constant)
+			t.Errorf("computed constant does not equal expected constant at index %d - want: %v, got: %v", i, want.ConstantsPool[i], constant)
 		}
 	}
 }
@@ -108,5 +114,32 @@ func TestCompileNumericTokens_UnaryExpressions(t *testing.T) {
 			t.Errorf("compilation error occurred: %s", err.Error())
 		}
 		assertBytecodeEquals(t, bytecode, tt.expectedBytecode)
+	}
+}
+
+func TestDiassembleBytecode(t *testing.T) {
+	tokens := []token.Token{
+		token.CreateLiteralToken(token.INT, int64(5), "5", 0, 0),
+		token.CreateToken(token.ADD, 0, 0),
+		token.CreateLiteralToken(token.INT, int64(15), "15", 0, 0),
+		token.CreateToken(token.EOF, 0, 0),
+	}
+	expected := `opcode: OP_CONSTANT, operand: 0, operand widths: 2 bytes, value: 5
+opcode: OP_CONSTANT, operand: 1, operand widths: 2 bytes, value: 15
+opcode: OP_ADD, operand: None, operand widths: 0 bytes`
+
+	compiler := NewCompiler(tokens)
+	_, err := compiler.Compile()
+	if err != nil {
+		t.Errorf("compilation error occurred: %s", err.Error())
+	}
+
+	result, err := compiler.DiassembleBytecode(false)
+	if err != nil {
+		t.Errorf("bytecode diassembly error: %s", err.Error())
+	}
+
+	if strings.TrimSpace(result) != strings.TrimSpace(expected) {
+		t.Errorf("\n\nwant:\n%s\n\ngot:\n%s", expected, result)
 	}
 }
