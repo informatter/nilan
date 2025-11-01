982eb531f3a75e448bdd9d02c180c5f9ec26d547
informatter <nicholas.rawitscher@gmail.com>
2025-11-01 22:25:41 +0000

    feat: Register emit command

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/google/subcommands"
 	"os"
+
+	"github.com/google/subcommands"
 )
 
 func main() {
-	subcommands.Register(subcommands.HelpCommand(), "")
-	subcommands.Register(subcommands.FlagsCommand(), "")
-	subcommands.Register(subcommands.CommandsCommand(), "")
-	subcommands.Register(&runCmd{}, "")
-	subcommands.Register(&replCmd{}, "")
+	// subcommands.Register(subcommands.HelpCommand(), "")
+	// subcommands.Register(subcommands.FlagsCommand(), "")
+	// subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&runCmd{}, "tree-walk-interpreter")
+	subcommands.Register(&replCmd{}, "tree-walk-interpreter")
+	subcommands.Register(&emitBytecodeCmd{}, "compiler")
 	flag.Parse()
 	ctx := context.Background()
 	os.Exit(int(subcommands.Execute(ctx)))
