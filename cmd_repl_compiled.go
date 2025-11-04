bdb1a6e2293858d009c3880f800bac816ad4f091
informatter <nicholas.rawitscher@gmail.com>
2025-11-04 21:38:26 +0000

    feat: Add repl command for the compiled version of nilan, with flags to diassemble bytecode and dump bytecode to a file

---
+++ cmd_repl_compiled.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"nilan/compiler"
+	"nilan/lexer"
+	"nilan/vm"
+
+	"github.com/google/subcommands"
+)
+
+type replCompiledCmd struct {
+	diassemble   bool
+	dumpBytecode bool
+}
+
+func (*replCompiledCmd) Name() string { return "cRepl" }
+func (*replCompiledCmd) Synopsis() string {
+	return "Start REPL session with the compiled version of nilan"
+}
+func (*replCompiledCmd) Usage() string {
+	return `nilan cRepl`
+}
+
+func (cmd *replCompiledCmd) SetFlags(f *flag.FlagSet) {
+	f.BoolVar(&cmd.diassemble, "diassemble", false, "diassemble the bytecode and dump it to a text file.")
+	f.BoolVar(&cmd.dumpBytecode, "dumpBytecode", false, "Writes the encoded bytecode as hexadecimal to a .nic file")
+	f.BoolVar(&cmd.diassemble, "di", false, "Shorthand for diassemble.")
+	f.BoolVar(&cmd.dumpBytecode, "du", false, "Shorthand for dumpBytecode")
+}
+
+func (cmd *replCompiledCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+
+	fmt.Println("\n\nWelcome to the compiled version of Nilan!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Fprintf(os.Stdout, ">>> ")
+		scanned := scanner.Scan()
+		if !scanned {
+			err := scanner.Err()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "💥 %s", err.Error())
+				return subcommands.ExitFailure
+			}
+		}
+
+		line := scanner.Text()
+		if line == "exit" {
+			os.Exit(0)
+		}
+		lex := lexer.New(line)
+		tokens, err := lex.Scan()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		compiler := compiler.New(tokens)
+		bytecode, err := compiler.Compile()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+		if cmd.diassemble {
+			_, err := compiler.DiassembleBytecode(true, "")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "💥 Bytecode diassemble error:\n:\t%s", err.Error())
+				return subcommands.ExitFailure
+			}
+
+		}
+		if cmd.dumpBytecode {
+			err := compiler.DumpBytecode("")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "💥 Dump bytecode error:\n:\t%s", err.Error())
+				return subcommands.ExitFailure
+			}
+		}
+		vm := vm.New()
+		runtimeErr := vm.Run(bytecode)
+		if runtimeErr != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return subcommands.ExitFailure
+		}
+	}
+}
--- main.go
+++ main.go
@@ -9,12 +9,10 @@ import (
 )
 
 func main() {
-	// subcommands.Register(subcommands.HelpCommand(), "")
-	// subcommands.Register(subcommands.FlagsCommand(), "")
-	// subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&runCmd{}, "tree-walk-interpreter")
 	subcommands.Register(&replCmd{}, "tree-walk-interpreter")
 	subcommands.Register(&emitBytecodeCmd{}, "compiler")
+	subcommands.Register(&replCompiledCmd{}, "compiler")
 	flag.Parse()
 	ctx := context.Background()
 	os.Exit(int(subcommands.Execute(ctx)))
