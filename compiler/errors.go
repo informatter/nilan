34e4236aae1881db450cc9b9fdd03d972b4f3655
informatter <nicholas.rawitscher@gmail.com>
2026-02-02 16:07:00 +0000

    feat: Add DeveloperError struct for development related errors

--- compiler/errors.go
+++ compiler/errors.go
@@ -9,3 +9,11 @@ type SyntaxError struct {
 func (e SyntaxError) Error() string {
 	return fmt.Sprintf("💥 SyntaxError: %s", e.Message)
 }
+
+type DeveloperError struct {
+	Message string
+}
+
+func (e DeveloperError) Error() string {
+	return fmt.Sprintf("🤖 DeveloperError: %s", e.Message)
+}
