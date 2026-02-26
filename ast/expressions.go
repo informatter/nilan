89d3d3fdcabe8be78ae92abeb55bf576d37db8bb
informatter <nicholas.rawitscher@gmail.com>
2026-02-26 16:04:07 +0000

    feat/refactor: Add `CallExpr` and renamed existing expression structs for clarity

--- ast/expressions.go
+++ ast/expressions.go
@@ -6,53 +6,53 @@ import (
 	"nilan/token"
 )
 
-// Binary represents a binary operation expression (e.g., "a + b").
+// BinaryExpr represents a binary operation expression (e.g., "a + b").
 // It consists of a left-hand side expression, an operator token (e.g., +, -, *, /),
 // and a right-hand side expression.
-type Binary struct {
-	Left     Expression  // The left-hand expression (e.g., "a" in "a + b")
+type BinaryExpr struct {
+	Left     Expr        // The left-hand expression (e.g., "a" in "a + b")
 	Operator token.Token // The operator (e.g., "+")
-	Right    Expression  // The right-hand expression (e.g., "b" in "a + b")
+	Right    Expr        // The right-hand expression (e.g., "b" in "a + b")
 }
 
-func (binary Binary) Accept(v ExpressionVisitor) any {
-	return v.VisitBinary(binary)
+func (binary BinaryExpr) Accept(v ExprVisitor) any {
+	return v.VisitBinaryExpr(binary)
 }
 
-// Unary represents a unary operation expression (e.g., "!a" or "-b").
+// UnaryExpr represents a unary operation expression (e.g., "!a" or "-b").
 // It consists of an operator token and a single right-hand expression.
-type Unary struct {
+type UnaryExpr struct {
 	Operator token.Token // The operator (e.g., "!" or "-")
-	Right    Expression  // The expression the operator is applied to (e.g., "a" or "b")
+	Right    Expr        // The expression the operator is applied to (e.g., "a" or "b")
 }
 
-func (unary Unary) Accept(v ExpressionVisitor) any {
-	return v.VisitUnary(unary)
+func (unary UnaryExpr) Accept(v ExprVisitor) any {
+	return v.VisitUnaryExpr(unary)
 }
 
-// Literal represents a literal value in the source code
+// LiteralExpr represents a literal value in the source code
 // (e.g., numbers, strings, booleans, or null).
-type Literal struct {
+type LiteralExpr struct {
 	Value any // The literal value (Go's `any` allows different possible types)
 }
 
-func (literal Literal) Accept(v ExpressionVisitor) any {
-	return v.VisitLiteral(literal)
+func (literal LiteralExpr) Accept(v ExprVisitor) any {
+	return v.VisitLiteralExpr(literal)
 }
 
-// Grouping represents a parenthesized expression (e.g., "(a + b)").
+// GroupingExpr represents a parenthesized expression (e.g., "(a + b)").
 // Useful for controlling evaluation precedence.
-type Grouping struct {
-	Expression Expression // The inner expression inside the parentheses
+type GroupingExpr struct {
+	Expression Expr // The inner expression inside the parentheses
 }
 
-func (grouping Grouping) Accept(v ExpressionVisitor) any {
-	return v.VisitGrouping(grouping)
+func (grouping GroupingExpr) Accept(v ExprVisitor) any {
+	return v.VisitGroupingExpr(grouping)
 }
 
-// Variable represents a value binded to a declared
+// VariableExpr represents a value binded to a declared
 // variable
-type Variable struct {
+type VariableExpr struct {
 	Name token.Token // An IDENTIFIER token
 }
 
@@ -62,11 +62,11 @@ type Variable struct {
 // Fields:
 //   - Name: The token corresponding to the variable's identifier. This is an
 //     IDENTIFIER token that holds the variable's name (lexeme).
-func (variable Variable) Accept(v ExpressionVisitor) any {
-	return v.VisitVariableExpression(variable)
+func (variable VariableExpr) Accept(v ExprVisitor) any {
+	return v.VisitVariableExpr(variable)
 }
 
-// Assign represents an assignment expression in the abstract syntax tree (AST).
+// AssignExpr represents an assignment expression in the abstract syntax tree (AST).
 // It models the operation of assigning a new value to an existing variable.
 //
 // Fields:
@@ -74,16 +74,16 @@ func (variable Variable) Accept(v ExpressionVisitor) any {
 //   - Value: The expression that produces the value being assigned to the variable.
 //     This can be any valid expression node in the AST, which will be
 //     evaluated and then stored in the environment.
-type Assign struct {
+type AssignExpr struct {
 	Name  token.Token
-	Value Expression
+	Value Expr
 }
 
-func (assign Assign) Accept(v ExpressionVisitor) any {
-	return v.VisitAssignExpression(assign)
+func (assign AssignExpr) Accept(v ExprVisitor) any {
+	return v.VisitAssignExpr(assign)
 }
 
-// Logical represents a logical expression in the abstract syntax tree (AST).
+// LogicalExpr represents a logical expression in the abstract syntax tree (AST).
 // It models the logical `if`, `else` expressions.
 //
 // Fields:
@@ -93,12 +93,31 @@ func (assign Assign) Accept(v ExpressionVisitor) any {
 //
 // Example:
 // >>> `if a<b or b>10 and b!=0`
-type Logical struct {
-	Left     Expression
+type LogicalExpr struct {
+	Left     Expr
 	Operator token.Token
-	Right    Expression
+	Right    Expr
 }
 
-func (logical Logical) Accept(v ExpressionVisitor) any {
-	return v.VisitLogicalExpression(logical)
+func (logical LogicalExpr) Accept(v ExprVisitor) any {
+	return v.VisitLogicalExpr(logical)
+}
+
+// CallExpr represents a function call expression in the abstract syntax tree (AST).
+// It models the invocation of a function with a set of arguments, or a function without arguments.
+// Example:
+// >>> `add(1, 2)` represents a call to the function `add` with the arguments `1` and `2`.
+type CallExpr struct {
+	// Callee is the expression representing the function being called.
+	// For example, in `add(1, 2)`, the callee is the variable expression `add`.
+	Callee Expr
+	// Parent is the token representing the closing parenthesis of the function call.
+	Paren token.Token
+	// Arguments is a slice of expressions representing the arguments passed to the function.
+	// For example, in `add(1, 2)`, the arguments are the literal expressions `1` and `2`.
+	Arguments []Expr
+}
+
+func (call CallExpr) Accept(v ExprVisitor) any {
+	return v.VisitCallExpr(call)
 }
