756a0ae3870199c599ff1a72f1cd1b882962a7f2
informatter <nicholas.rawitscher@gmail.com>
2026-02-26 16:05:20 +0000

    feat: Add `FunctionStmt`, `ReturnStmt` AST node's and renamed existing

--- ast/statements.go
+++ ast/statements.go
@@ -8,7 +8,7 @@ import "nilan/token"
 // Example: `foo + bar;`
 // This evaluates the expression and discards the result.
 type ExpressionStmt struct {
-	Expression Expression // The expression used as a statement
+	Expression Expr // The expression used as a statement
 }
 
 func (e ExpressionStmt) Accept(v StmtVisitor) any {
@@ -18,7 +18,7 @@ func (e ExpressionStmt) Accept(v StmtVisitor) any {
 // PrintStmt represents a print statement that outputs the result
 // of evaluating an expression. Example: `print foo + bar;`
 type PrintStmt struct {
-	Expression Expression // The expression whose result will be printed
+	Expression Expr // The expression whose result will be printed
 }
 
 func (p PrintStmt) Accept(v StmtVisitor) any {
@@ -36,7 +36,7 @@ type VarStmt struct {
 	// Initialiser is the expression assigned to the variable when declared.
 	// For example, `var x=5` the initialser is `5`. Since this is an expression,
 	// this is also supported `var x = 5+3`.
-	Initializer Expression
+	Initializer Expr
 }
 
 func (varStmt VarStmt) Accept(v StmtVisitor) any {
@@ -57,7 +57,7 @@ func (blockStmt BlockStmt) Accept(v StmtVisitor) any {
 // to evaluate the statement to execute if the expression is true
 // or the statement to execute of the expression is false.
 type IfStmt struct {
-	Condition Expression
+	Condition Expr
 	Then      Stmt
 	Else      Stmt
 }
@@ -74,10 +74,41 @@ func (stmt IfStmt) Accept(v StmtVisitor) any {
 //     otherwise, the loop terminates.
 //   - Body: The block statement representing the loop body,
 type WhileStmt struct {
-	Condition Expression
+	Condition Expr
 	Body      Stmt
 }
 
 func (stmt WhileStmt) Accept(v StmtVisitor) any {
 	return v.VisitWhileStmt(stmt)
+}
+
+// FunctionStmt represents a function declaration statement, which consists of the function's name,
+// its parameters, and its body.
+type FunctionStmt struct {
+
+	// Name is the token representing the function's identifier.
+	Name token.Token
+	// Body is the block statement representing the function's body, which contains the statements that define the
+	// function's behavior.
+	Body BlockStmt
+	// Parameters is a list of tokens representing the function's parameters.
+	Parameters []token.Token
+}
+
+func (stmt FunctionStmt) Accept(v StmtVisitor) any {
+	return v.VisitFunctionStmt(stmt)
+}
+
+// ReturnStmt represents a return statement, which consists of the return keyword and the expression whose value
+// is to be returned.
+type ReturnStmt struct {
+	// Keyword is the token representing the 'return' keyword.
+	Keyword token.Token
+	// Value is the expression whose value is to be returned when the return statement is executed.
+	// TODO: Add handling for returning functions.
+	Value Expr
+}
+
+func (stmt ReturnStmt) Accept(v StmtVisitor) any {
+	return v.VisitReturnStmt(stmt)
 }
