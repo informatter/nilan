7f0a6d7f8862fb3a3d2e0c704530c6dcaf227124
informatter <nicholas.rawitscher@gmail.com>
2026-02-27 15:51:01 +0000

    refactor: Update comments to clarify Go syntax error handling and remove unused command registrations

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 	"github.com/google/subcommands"
 )
 
+// func foo(){}
+// foo()
+// NOTE 💡: If the above code is uncommented, the following Go syntax error will be raised when attempting to
+// run this file: `syntax error: non-declaration statement outside function body`
+// Even though the error that is actually being raised in the Go parses is `expected declaration, found foo`
+// It will be good to have the same type of error handling in Nilan.
 func main() {
-	subcommands.Register(&runCmd{}, "tree-walk-interpreter")
-	subcommands.Register(&replCmd{}, "tree-walk-interpreter")
 	subcommands.Register(&emitBytecodeCmd{}, "compiler")
 	subcommands.Register(&replCompiledCmd{}, "compiler")
 	subcommands.Register(&runCompiledCmd{}, "compiler")
 	flag.Parse()
 	ctx := context.Background()
 	os.Exit(int(subcommands.Execute(ctx)))
+
 }
